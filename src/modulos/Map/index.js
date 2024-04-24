321f95a23bead4eca0ab81f65dd55684490d000c
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-04-24 17:00:28 -0300

    responsividade para celular

--- src/modulos/Map/index.js
+++ src/modulos/Map/index.js
@@ -4,15 +4,16 @@ import './Mapa.css';
 const Mapa = ({lista}) =>{
     return(
         <div className='mapa'>
-            <div className='mapa-texto'>
-
-                <h4>Club Map</h4>
-                <ul>
-                    {lista.map(lugar => <li key = {lugar}>{lugar}</li>)}
-                </ul>
-                
+            <h4 className='titulo'>Club Map</h4>
+            <div className='mapa-container'>
+                <div className='mapa-texto'>
+                    <ul>
+                        {lista.map(lugar => <li key = {lugar}>{lugar}</li>)}
+                    </ul>
+                </div>
+                <img src='/mapa.webp' alt='imagem mapa do clube'/>
             </div>
-            <img src='/mapa.webp' alt='imagem mapa do clube'/>
+            
         </div>
     )
 }
