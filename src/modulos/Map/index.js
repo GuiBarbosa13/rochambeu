625b703f4689c326667eb5e29ebd2824903facb5
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-04-25 11:21:16 -0300

    melhorando a responsividade para celular

--- src/modulos/Map/index.js
+++ src/modulos/Map/index.js
@@ -7,6 +7,7 @@ const Mapa = ({lista}) =>{
             <h4 className='titulo'>Club Map</h4>
             <div className='mapa-container'>
                 <div className='mapa-texto'>
+                    <h4 className='titulo-lg'>Club Map</h4>
                     <ul>
                         {lista.map(lugar => <li key = {lugar}>{lugar}</li>)}
                     </ul>
